c3e48ce0d3ebd1500514c703b820134c9f8d6bc8
kingmohammas <johannes.maendle@hpe.com>
2018-12-27 13:37:09 +0100

    Add Header component

---
+++ pdm/src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div id="Header">
+        <h2 id="Header-label"> Package Delivery Marketplace </h2>
+      </div>
+    );
+  }
+}
+
+export default Header;
